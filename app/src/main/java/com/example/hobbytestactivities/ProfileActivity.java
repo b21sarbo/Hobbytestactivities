5a5bc9e1355dbf6ea6d50de5eea263206dd62a59
Sara Borg <102578759+b21sarbo@users.noreply.github.com>
2022-04-12 15:24:00 +0200

    signout button

--- app/src/main/java/com/example/hobbytestactivities/ProfileActivity.java
+++ app/src/main/java/com/example/hobbytestactivities/ProfileActivity.java
@@ -3,12 +3,35 @@ package com.example.hobbytestactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Log.d("==>", "onCreate");
     }
+
 }
