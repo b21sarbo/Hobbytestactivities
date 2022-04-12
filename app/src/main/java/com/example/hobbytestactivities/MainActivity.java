edc29d8ccff5414cc12b65145083332146e5c08f
Sara Borg <102578759+b21sarbo@users.noreply.github.com>
2022-04-12 14:17:01 +0200

    variable

--- app/src/main/java/com/example/hobbytestactivities/MainActivity.java
+++ app/src/main/java/com/example/hobbytestactivities/MainActivity.java
@@ -4,37 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signin;
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
-        Log.d("pil","onResume");
+        Log.d("pil","-onResume");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("pil","onStart");
+        Log.d("pil","-onStart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("pil","onStop");
+        Log.d("pil","-onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("pil","onDestroy");
+        Log.d("pil","-onDestroy");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("pil","onCreate");
+        Log.d("pil","-onCreate");
+        signin = findViewById(R.id.signin_button);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
