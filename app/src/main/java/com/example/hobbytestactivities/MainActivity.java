d213108a1c5026f9b2a7a305ddd90df4c7d37d4a
Sara Borg <102578759+b21sarbo@users.noreply.github.com>
2022-04-12 13:54:11 +0200

    Added log of main activity life cycle state

--- app/src/main/java/com/example/hobbytestactivities/MainActivity.java
+++ app/src/main/java/com/example/hobbytestactivities/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.hobbytestactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("pil","onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("pil","onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("pil","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("pil","onDestroy");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("pil","onCreate");
     }
 }
